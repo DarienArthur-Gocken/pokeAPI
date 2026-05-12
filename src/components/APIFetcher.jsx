5a704f94ae095f2ea206990a8dd6c331573455c3
Lilian <210286008+liliann19@users.noreply.github.com>
2026-05-12 10:49:58 -0700

    get pokemon name

--- src/components/APIFetcher.jsx
+++ src/components/APIFetcher.jsx
@@ -2,13 +2,28 @@ import { useState, useEffect } from "react"
 
 export default function APIFetcher() {
     const [APIData, setAPIData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon/ditto")
             .then(res => res.json())
-            .then(data => setAPIData(data))
+            .then(data => {
+                setAPIData(data)
+                setLoading(false)
+            })
     }, [])
 
+    if (loading) return <p>Loading...</p>
+
     console.log(APIData);
+
+    function getName() {
+        const pokemonName = APIData.name
+        return <p>{pokemonName}</p>
+    }
+
+    return (
+        <div>{ getName() }</div>
+    )
 }
 
